9d14dea72e4d995b2f7874f1883172004e85a9d0
GustavoCoelho295 <iago.pinheirodeoliveira@gmail.com>
2025-10-10 11:54:13 -0300

    ajustado navbar para telas maiores

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { List } from "@phosphor-icons/react";
+import BudgetButton from "./buttons/BudgetButton";
 
 export default function Navbar() {
   return (
     <nav className="w-full h-14 flex items-center content-center px-6 shadow-md">
-      <div className="flex items-center w-full gap-3">
+      <div className="flex items-center w-full sm:w-3xs gap-3">
         <img src="logo.png" className="size-8" />
         <h1 className="text-xs font-bold">IP Soluções Digitais</h1>
       </div>
-      <List size={32} color="#3747be" weight="bold" />
+      <div className="hidden sm:flex flex-row w-full items-center justify-between text-xs font-bold">
+        <div className="pl-2 flex flex-row items-center gap-4">
+          <h1>Sobre</h1>
+          <h1>Nossos Serviços</h1>
+          <h1>Contato</h1>
+        </div>
+        <BudgetButton />
+      </div>
+      <List size={32} color="#3747be" weight="bold" className="sm:hidden" />
     </nav>
   );
 }
---
+++ src/components/buttons/BudgetButton.jsx
@@ -0,0 +1,7 @@
+export default function BudgetButton() {
+    return(
+        <button className="bg-blue-700 px-2 py-1 border-none rounded-sm font-medium text-white">
+            Solicitar orçamento grátis
+        </button>
+    )
+}
