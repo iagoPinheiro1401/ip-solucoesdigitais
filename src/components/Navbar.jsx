807e0033116db6e11fb668f6ce021bc3a5979d2f
GustavoCoelho295 <iago.pinheirodeoliveira@gmail.com>
2025-10-10 12:20:54 -0300

    ajustado navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,17 @@ import BudgetButton from "./buttons/BudgetButton";
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-14 flex items-center content-center px-6 shadow-md">
-      <div className="flex items-center w-full sm:w-3xs gap-3">
+    <nav className="w-full h-14 flex items-center justify-between px-6 shadow-md">
+      <div className="flex items-center gap-3">
         <img src="logo.png" className="size-8" />
         <h1 className="text-xs font-bold">IP Soluções Digitais</h1>
       </div>
-      <div className="hidden sm:flex flex-row w-full items-center justify-between text-xs font-bold">
-        <div className="pl-2 flex flex-row items-center gap-4">
+        <div className="hidden sm:flex flex-row items-center gap-4 text-xs font-bold">
           <h1>Sobre</h1>
           <h1>Nossos Serviços</h1>
           <h1>Contato</h1>
         </div>
-        <BudgetButton />
-      </div>
+      <BudgetButton className="hidden sm:flex"/>
       <List size={32} color="#3747be" weight="bold" className="sm:hidden" />
     </nav>
   );
--- src/components/buttons/BudgetButton.jsx
+++ src/components/buttons/BudgetButton.jsx
@@ -1,6 +1,6 @@
-export default function BudgetButton() {
+export default function BudgetButton({ className }) {
     return(
-        <button className="bg-blue-700 px-2 py-1 border-none rounded-sm font-medium text-white">
+        <button className={`bg-blue-700 px-2 py-1 border-none rounded-sm text-sm font-medium text-white ${className}`}>
             Solicitar orçamento grátis
         </button>
     )
